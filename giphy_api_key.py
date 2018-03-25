b6b7038048d1161a70c200de40e93df4228b2ede
Yuting Wu <phillng0073@Yutings-MacBook-Pro.local>
2018-03-24 23:03:01 -0400

    push all the origin files

---
+++ giphy_api_key.py
@@ -0,0 +1,17 @@
+api_key = "" # Fill in your API key here.
+
+# TODO 364: To get a Giphy API key to paste here,
+# Go to https://developers.giphy.com/
+# Log in if you already have an account, or create an account.
+# It is OK to log in with Facebook, but you can also create an account or even create an account you intend to delete after this assignment that uses a brand new Gmail email, if you prefer to do that.
+# Click: Create an App
+# You should see a text box that looks like this: https://www.dropbox.com/s/2czgm69j1k0fzfs/Screenshot%202018-03-12%2012.15.30.png?dl=0
+# Enter any name for your app name. For the brief App Description you can enter that you are using it for a class project.
+# Then click the Create New App button.
+# You should be brought to a new screen, where your API key, a long string of random letters and numbers, will show up. This is all you need -- copy this and paste it into the above quotations! MAKE SURE you follow the rest of the instructions in this file before making any other commits to git.
+
+# TODO 364: Add this file name:
+# giphy_api_key.py
+# to the included .gitignore file.
+# Then save, add, and commit the .gitignore file to YOUR git repository.
+# That means Git won't let you commit THIS file any longer -- so when the assignment is graded, we will use our own, not yours.
